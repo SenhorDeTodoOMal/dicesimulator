42e06dbc9f86f4a881a489f44ec295f036115137
SenhorDeTodoOMal <senhordetodoomal@gmail.com>
2016-05-13 00:06:04 +0100

    ignore

--- src/main/java/pt/fts/enear/jprog/dice/D6.java
+++ src/main/java/pt/fts/enear/jprog/dice/D6.java
@@ -1,12 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * LICENSE
  */
 package pt.fts.enear.jprog.dice;
 
 /**
- * This class is an implementation of @link Dice
+ * This class is an implementation of Die that
+ * represents the traditional 6 sided die which
+ * is a perfect cube with numbered faces
+ * 
+ * @see Die
  * @author Francisco Tiago Soares
  */
 public class D6 implements Die<Integer>{
